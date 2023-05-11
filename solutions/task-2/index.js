7b7c07bfd21222bff27764071d1fe151d38d636a
vadymprokhorchuk <116506719+vadymprokhorchuk@users.noreply.github.com>
2023-05-11 15:10:26 +0300

    Create index.js

---
+++ solutions/task-2/index.js
@@ -0,0 +1,14 @@
+function getCount(str) {
+  let vowelsCount = 0;
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  for (let char of str) {
+    if (vowels.includes(char)) {
+      vowelsCount++;
+    }
+  }
+
+  return vowelsCount;
+}
+
+console.log(getCount('heeallo')); // 2
